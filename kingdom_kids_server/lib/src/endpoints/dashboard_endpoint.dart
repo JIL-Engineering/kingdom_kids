e5e3172c8d687be3077a0b3a96d569838c574b12
Daniel Dev <landrytchamga@gmail.com>
2026-08-21 15:14:09 +0000

    creation des endpoints pour le sprint 1

---
+++ kingdom_kids_server/lib/src/endpoints/dashboard_endpoint.dart
@@ -0,0 +1,3 @@
+import 'package:serverpod/serverpod.dart';
+
+class DashboardEndpoint extends Endpoint {}
---
+++ kingdom_kids_server/lib/src/endpoints/devotional_endpoint.dart
@@ -0,0 +1,3 @@
+import 'package:serverpod/serverpod.dart';
+
+class DevotionalEndpoint extends Endpoint {}
