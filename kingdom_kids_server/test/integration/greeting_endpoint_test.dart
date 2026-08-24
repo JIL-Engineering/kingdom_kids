093478e34b92ff08e7aafb257264b808b737c5e4
sterelle <bellaladouce6@gmail.com>
2026-08-24 21:50:02 +0100

    creation du fichier greeting_endpoint_test.dart et  test

---
+++ kingdom_kids_server/test/integration/greeting_endpoint_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Greeting Endpoint', () {
+    test('should return a greeting', () {
+      // Add your test implementation here
+      expect(true, isTrue);
+    });
+  });
+}
